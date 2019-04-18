40a8044416481ff567c86af4971b15f930974bf8
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-04-18 18:12:54 -0500

    histotia 1 terminada, pasa test 100%

--- src/main.js
+++ src/main.js
@@ -2,52 +2,53 @@
 const containerCharacters = document.getElementById("container-characters");
 
 const data = () => {
-    fetch('https://raw.githubusercontent.com/farfanjackelyne9/LIM009-DL-2.0/master/src/data/potter.json')
-        .then(res => res.json())
-        .then(datos => {
-            paintData(datos)
-        })
-}
+  fetch('https://raw.githubusercontent.com/farfanjackelyne9/LIM009-DL-2.0/master/src/data/potter.json')
+    .then(res => res.json())
+    .then(datos => {
+      paintData(datos);
+    });
+};
 data();
 const paintData = (datos) => {
-    let newData = "";
-    for (let i = 0; i < datos.length; i++) {
-        newData += `
+  let newData = "";
+  for (let i = 0; i < datos.length; i++) {
+    newData += `
         <section id="box-characters" class="col-xs-6 col-md-4 col-sm-6 col-lg-4">
-         <section id="box-act">`
-         if(datos[i].house === "Gryffindor" ){
-          newData += `<div><img id="bander" src="imagenes/bandera-roja.JPG"</div>`
-        }
-        if(datos[i].house === "Slytherin" ){
-          newData += `<div><img id="bander" src="imagenes/bandera-verde.JPG"</div>`
-        }
-        if(datos[i].house === "Ravenclaw" ){
-          newData += `<div><img id="bander" src="imagenes/bandera-azul.JPG"</div>`
-        }
-        if(datos[i].house === "Hufflepuff" ){
-          newData += `<div><img id="bander" src="imagenes/bandera-amarilla.JPG"</div>`
-        }
-         newData +=`<img  id="img-act" src="${datos[i].image}"></img>
-           </br>
+         <section id="box-act">`;
+    if (datos[i].house === "Gryffindor") {
+      newData += `<div ><img class="bander" src="imagenes/bandera-roja.JPG"</div>`;
+    }
+    if (datos[i].house === "Slytherin") {
+      newData += `<div ><img class="bander" src="imagenes/bandera-verde.JPG"</div>`;
+    }
+    if (datos[i].house === "Ravenclaw") {
+      newData += `<div ><img class="bander" src="imagenes/bandera-azul.JPG"</div>`;
+    }
+    if (datos[i].house === "Hufflepuff") {
+      newData += `<div ><img class="bander" src="imagenes/bandera-amarilla.JPG"</div>`;
+    }
+    if (datos[i].house === "") {
+      newData += `<div ><img class="bander" src="imagenes/bandera-vacia.JPG"</div>`;
+    }
+    newData += `</br><img  id="img-act" src="${datos[i].image}"></img></br>
            <strong><p class="tittle-box">${datos[i].name}</p></strong>
-           <p class="tittle-box">${datos[i].house}</p>
-           </br>
-         <div>
-           Especie
-           </br>
-           <strong>${datos[i].species}</strong>
-           </br>`
-           if (typeof datos[i].yearOfBirth === "number") {
-            newData += `<div>Edad</br><strong>${2019 - datos[i].yearOfBirth}</strong></div>`;
-          } else {
-            newData += `<div>Edad</br><strong>${"No encontrado"}</strong></div>`;
-          }
-           newData +=`Actor
-           </br>
-           <strong>${datos[i].actor}</strong>
+           <p class="tittle-box">${datos[i].house}</p></br>
+         <div><img class="icon" src="icono/icono-especies.jpeg"></img>Especie</br><strong><p class="data">${datos[i].species}</p></strong></br>`;
+    if (datos[i].hogwartsStudent === true) {
+      newData += `</br><img class="icon" src="icono/icono-estudiantes.jpeg">Rol</br><strong><p class="data">${"Estudiante"}</p></strong></br>`;
+    }
+    if (datos[i].hogwartsStudent === false) {
+      newData += `</br><img class="icon" src="icono/icono-estudiantes.jpeg">Rol</br><strong><p class="data">${"Staff"}</p></br></strong>`;
+    }
+    if (typeof datos[i].yearOfBirth === "number") {
+      newData += `</br><img class="icon" src="icono/icono-edad.jpeg">Edad</br><strong><p class="data">${2019 - datos[i].yearOfBirth}</p></strong></br>`;
+    } else {
+      newData += `</br><img class="icon" src="icono/icono-edad.jpeg">Edad</br><strong><p class="data">${"No encontrado"}</p></strong></br>`;
+    }
+    newData += `</br><img class="icon" src="icono/icono-actores.jpeg">Actor</br><strong><p class="data">${datos[i].actor}</p></strong></br></br>
          </div>
          </section>
         </section>`;
-    }
-    containerCharacters.innerHTML = newData;
-}
+  }
+  containerCharacters.innerHTML = newData;
+};
