392eacbc0889dd6a05e11d7c63084d71b71eda6f
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-04-18 01:34:05 -0500

    pintado de todas las caracteristicas

--- src/main.js
+++ src/main.js
@@ -5,36 +5,30 @@ const data = () => {
   fetch('https://raw.githubusercontent.com/farfanjackelyne9/LIM009-DL-2.0/master/src/data/potter.json')
     .then(res => res.json())
     .then(datos => {
-      paintData(datos);
+      paintData(datos)
     });
 };
 data();
 const paintData = (datos) => {
   let newData = "";
   for (let i = 0; i < datos.length; i++) {
-    newData += `
-        <section id="box-characters" class="col-xs-6 col-md-4 col-sm-6 col-lg-4">
-         <section id="box-act">
-           <img  id="img-act" src="${datos[i].image}"></img>
-           </br>
-           <strong><p class="tittle-box">${datos[i].name}</p></strong>
-           <p class="tittle-box">${datos[i].house}</p>
-           </br>
-         <div>
-           Especie
-           </br>
-           <strong>${datos[i].species}</strong>
-           </br>
-           Edad
-           </br>
-           <strong>${2019 - datos[i].yearOfBirth}</strong>
-           </br>
-           Actor
-           </br>
-           <strong>${datos[i].actor}</strong>
-         </div>
-         </section>
-        </section>`;
+    newData += `<img  id="img-act" src="${datos[i].image}"></img>
+           <strong><p>${datos[i].name}</p></strong>
+           <p>${datos[i].house}</p>
+           Especie</br>
+           <strong>${datos[i].species}</strong></br>`
+    if (datos[i].hogwartsStudent === true) {
+      newData += `Rol</br><strong>${"Estudiante"}</strong>`
+    }
+    if (datos[i].hogwartsStudent === false) {
+      newData += `Rol</br><strong>${"Staff"}</br></strong>`
+    }
+    if (typeof datos[i].yearOfBirth === "number") {
+      newData += `<div>Edad</br><strong>${2019 - datos[i].yearOfBirth}</strong></div>`;
+    } else {
+      newData += `<div>Edad</br><strong>${"No encontrado"}</strong></div>`;
+    }
+    newData += `Actor</br><strong>${datos[i].actor}</strong>`;
   }
   containerCharacters.innerHTML = newData;
 };
