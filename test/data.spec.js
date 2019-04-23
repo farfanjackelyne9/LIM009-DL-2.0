999e8255792093bfa911b8d24ccb5a6b41e66c97
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-04-23 01:12:53 -0500

    detallando el data.spect, imagen centrada

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,14 +1,29 @@
 global.window = global;
 require('../src/data');
 require('./data.spec.js');
-
+const inputGen = [{ name: "Hermayone", gender: "female"}, { name: "Harry Potter", gender: "male"}];
+const conditionGen = "male";
+const outputGen = [{ name: "Harry Potter", gender: "male"}];
+const inputHom = [{ name: "Hermayone", house: "Gryffindor"}, { name: "Draco Malfoy", house: "Slytherin"}];
+const conditionHom = "Slytherin";
+const outputHom = [{ name: "Draco Malfoy", house: "Slytherin"}];
+const inputStud = [{ name: "Draco Malfoy", hogwartsStudent: true}, { name: "Minerva McGonagall", hogwartsStudent: false }];
+const conditionStud = "student";
+const outputStud = [{ name: "Draco Malfoy", hogwartsStudent: true}];
+const inputStaf = [{ name: "Draco Malfoy", hogwartsStaff: false}, { name: "Minerva McGonagall", hogwartsStaff: true }];
+const conditionStaf = "staff";
+const outputStaf = [{ name: "Minerva McGonagall", hogwartsStaff: true }];
+const inputSort1 = [{ "yearOfBirth": 1990 }, { "yearOfBirth": 1800 }];
+const outputSort1 = [{"yearOfBirth": 1800}, {"yearOfBirth": 1990}];
+const inputSort2 = [{"yearOfBirth": 1800}, {"yearOfBirth": 1800}];
+const outputSort2 = [{"yearOfBirth": 1800}, {"yearOfBirth": 1800}];
 describe('filterGen', () => {
   it('debería ser una función', () => {
     expect(typeof Global.filterGen).toBe('function');
   });
 
   it('Esta funcion deberia filtrar por genero', () => {
-    expect(Global.filterGen([{ gender: "male" }, { gender: "female" }, { name: "Harry Potter", gender: "male" }], "")).toEqual([]);
+    expect(Global.filterGen(inputGen, conditionGen)).toEqual(outputGen);
   });
 });
 describe('filterHom', () => {
@@ -17,7 +32,7 @@ describe('filterHom', () => {
   });
 
   it('Esta funcion deberia filtrar por casas', () => {
-    expect(Global.filterHom([{ house: "Slytherin" }, { house: "Ravenclaw" }, { name: "Harry Potter", house: "Gryffindor" }], "")).toEqual([]);
+    expect(Global.filterHom(inputHom, conditionHom)).toEqual(outputHom);
   });
 });
 describe('filterStud', () => {
@@ -26,7 +41,7 @@ describe('filterStud', () => {
   });
 
   it('Esta funcion deberia filtrar por estudiantes', () => {
-    expect(Global.filterStud([{ estudiante: "Harry" }, { estudiante: "Hermayone" }], "")).toEqual([]);
+    expect(Global.filterStud(inputStud, conditionStud)).toEqual(outputStud);
   });
 });
 describe('filterStaff', () => {
@@ -35,7 +50,7 @@ describe('filterStaff', () => {
   });
 
   it('Esta funcion deberia filtrar por staff', () => {
-    expect(Global.filterStaff([{ staff: "Argus" }, { name: "Harry Potter", house: "Gryffindor" }], "")).toEqual([]);
+    expect(Global.filterStaff(inputStaf, conditionStaf)).toEqual(outputStaf);
   });
 });
 describe('sortData', () => {
@@ -44,10 +59,10 @@ describe('sortData', () => {
   });
 
   it('debería retornar un array ordenado ', () => {
-    expect(Global.sortData([{ "yearOfBirth": 1990 }, { "yearOfBirth": 1800 }])).toEqual([{"yearOfBirth": 1800}, {"yearOfBirth": 1990}]);
+    expect(Global.sortData(inputSort1)).toEqual(outputSort1);
   });
 
-  it('debería retornar un array ordenado ', () => {
-    expect(Global.sortData([{"yearOfBirth": 1800}, {"yearOfBirth": 1800}])).toEqual([{ "yearOfBirth": 1800 }, { "yearOfBirth": 1800 }]);
+  it('debería retornar el mismo array ', () => {
+    expect(Global.sortData(inputSort2)).toEqual(outputSort2);
   });
 });
